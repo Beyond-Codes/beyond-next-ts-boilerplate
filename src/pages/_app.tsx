72d6ddd84ccf9ad825f7853d9a67911c2f94405e
JAEGER_ <omer.kayalar0101@icloud.com>
2023-02-22 23:07:33 +0300

    feat: init localization

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import PopupWrapper from '@/contexts/Popup.context';
 import AlertPopup from '@/components/popups/Alert.popup';
+import LanguageWrapper from '@/contexts/Language.context';
 import ConfirmPopup from '@/components/popups/Confirm.popup';
+
+// ? Global styles
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <PopupWrapper>
-      <AlertPopup />
-      <ConfirmPopup />
-      <Component {...pageProps} />
-    </PopupWrapper>
+    <LanguageWrapper>
+      <PopupWrapper>
+        <AlertPopup />
+        <ConfirmPopup />
+        <Component {...pageProps} />
+      </PopupWrapper>
+    </LanguageWrapper>
   );
 }
