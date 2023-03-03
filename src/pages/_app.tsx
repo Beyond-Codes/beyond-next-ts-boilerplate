74822daf17e2257c14cfd8dd67b5a702b58920b5
JAEGER_ <omer.kayalar0101@icloud.com>
2023-03-03 11:49:25 +0300

    feat: local font implementation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,55 @@
 import React from 'react';
-import type { AppProps } from 'next/app';
 import PopupWrapper from '@/contexts/Popup.context';
 import AlertPopup from '@/components/popups/Alert.popup';
 import LanguageWrapper from '@/contexts/Language.context';
 import ConfirmPopup from '@/components/popups/Confirm.popup';
 
+import type { AppProps } from 'next/app';
+
 // ? Global styles
 import '@/styles/globals.css';
 
+// * Local font implementation - #1
+//
+// import localFont from '@next/font/local';
+//
+// const Poppins = localFont({
+//   src: [
+//     {
+//       path: '../../public/assets/fonts/Poppins-Thin.otf',
+//       weight: '100',
+//     },
+//    ...
+//   ],
+//   variable: '--font-poppins',
+//   fallback: ['Helvetica', 'ui-sans-serif'],
+// });
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    // * Local font implementation - #2
+    //
+    // {/* eslint-disable-next-line react/no-unknown-property */}
+    //   <style jsx global>
+    //     {`
+    //       :root {
+    //         --font-poppims: ${Poppins.style.fontFamily};
+    //       }
+    //     `}
+    //   </style>
+    //
+    // * Google Analytics implementation
+    //
+    //   <Script id="google-analytics" strategy="afterInteractive">
+    //      {`
+    //        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    //        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    //        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    //        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    // !      ga('create', 'YOUR-GA-CODE', 'auto');
+    //        ga('send', 'pageview');
+    //      `}
+    //    </Script>
     <LanguageWrapper>
       <PopupWrapper>
         <AlertPopup />
