62ff647255776827e5db5a45ad1e92c00a40ccdb
JAEGER_ <omer.kayalar0101@icloud.com>
2023-03-20 14:57:04 +0300

    feat: handle errors

--- src/common/helpers/api.helper.ts
+++ src/common/helpers/api.helper.ts
@@ -1,6 +1,7 @@
 import { Axios } from 'axios';
 import API from '@/common/configs/api.config';
 import { ApiHelperClass } from '@/types/helper.types';
+import HelperError from '../handlers/error.handler';
 
 class ApiHelper implements ApiHelperClass {
   public readonly API: Axios;
@@ -9,21 +10,13 @@ class ApiHelper implements ApiHelperClass {
 
   constructor() {
     this.API = API;
-    this.defaultError = '😕 Oops, something went wrong.';
-  }
-
-  errorHandler(error: any): string {
-    return (
-      error?.response?.data?.message ||
-      error?.response?.data?.text ||
-      this.defaultError
-    );
+    this.defaultError = 'errors.default';
   }
 
   public async getCountries() {
     try {
       const { data } = await this.API.get('/countries').catch((error) => {
-        throw new Error(this.errorHandler(error));
+        throw new HelperError(this.errorHandler(error));
       });
 
       return {
@@ -36,6 +29,27 @@ class ApiHelper implements ApiHelperClass {
         error: error?.message || this.defaultError,
       };
     }
+  }
+
+  errorHandler(error: any): { message: string; code?: string | number } {
+    const errorResponse = error?.response?.data;
+    const defaultError = error;
+
+    const message = errorResponse?.message || this.defaultError;
+    const code = errorResponse?.code || defaultError?.code;
+
+    // Implement your own error handling here
+    // if (code === 'USERNOTFOUND') {
+    //   return {
+    //     message: 'errors.userNotFound',
+    //     code,
+    //   };
+    // }
+
+    return {
+      message,
+      code,
+    };
   }
 }
 
